223d1253759750cd182de4ac6f8f19b108e2506e
Dumb1protogen <ghjcntvf@mail.ru>
2023-03-19 00:13:01 +0600

    Sooo... close to end butt still need some fix

--- pt.js
+++ pt.js
@@ -4,17 +4,20 @@
 let vet = false; 
 //4
 
-let mayor = 0;
+let mayor = false;
 //7
-let gf = 0;
-//
+let gf = false;
+//10
+let mafioso = 0;
+//10 and 9
 
 let a = 0;
+let i = 0
 
 let random_role = function () {
-a = Math.floor(Math.random() * 7) + 1;
-if (a != 4 && a != 7){
-   console.log (i + "" + a);
+a = Math.floor(Math.random() * 15) + 1;
+if (a != 4 && a != 7 && a != 10 && a != 9){
+   console.log (i + " you are " + a);
    i++
    return i;
 }
@@ -22,19 +25,25 @@ if (a == 4 ) {
     if (vet == false){
     vet = true;
     i++
-    console.log (i + "" + a);
-    return vet;
-    return i;}
-    } else if (vet == true){
-        console.log ("agian");
+    console.log (i + " you are " + a);
+    return vet, i;}
+    } else {
+        random_role;
     } 
     if (a == 7){
     if (mayor == false){
         mayor= true;
         i++
         console.log (a)
-        return mayor;
-        return i;
+        return mayor, i;
+    }
+    if (a == 10);{
+        if (gf == false){
+            gf = true 
+
+            console.log (i + " you are " + a)
+            return gf, i;
+        }
     }
     }
    
@@ -42,23 +51,65 @@ if (a == 4 ) {
     }
     do{
     random_role();
-    }while (i<20);
-    console.log ("test");
-    console.log (vet);
+    }while (i != 16);
+
 //TO DO switch 
-switch (random role){
+
+
+
+
+
+
+
+
+/*
+let i = 0
+
+let random_role = function () {
+    a = Math.floor(Math.random() * 14) + 1;}
+
+
+
+
+
+random_role 
+alert (a);
+
+switch (random_role){
 case 4:
+    if (vet == false){
+
 vet = true
 console.log (a);
-i++
+i = i + 1
 return i;
+    }
+else random_role;
 break;
 case 7:
-break;
-case 999:
-break;
-default 
+    if (mayor == false) {
+
+mayor = true
 console.log (a);
-i++
+i = i + 1 
+}
+else random_role
+    
+break;
+case 10:
+    if (vet == false){
+
+vet = true
+console.log (a);
+i = i + 1
+return i;
+    }
+else random_role;
+    
+break;
+default: 
+console.log (a);
+i = i + 1
 return i;
 }
+*/
