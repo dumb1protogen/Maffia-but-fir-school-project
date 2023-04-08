888dddf3b28673044533c59223316fa58db0b7b3
Dumb1protogen <ghjcntvf@mail.ru>
2023-04-08 11:54:00 +0600

    Next time

--- generate role (switch).js
+++ generate role (switch).js
@@ -1,31 +1,49 @@
-let Sitizen = false
-let a = 0
-let i = 0
+let pool_role = {     
+    Sleeper: 0,  
+    Maf: 0        
+  };
 
-random_role = function () {
-    a = Math.floor(Math.random() * 2) + 1;}
 
-do {
 
-    random_role() 
+  let i =  0
+do { // here start
 
-    console.log (a + " debug"); 
 
-    switch (a) {
-        case 1:
-            if (Sitizen < 3 )  
-            { 
-        Sitizen++
-        i++  
-        console.log ("given");
-    }
-    case 2: 
-    
-    break
-        default:
-            console.log ("another");
+
+//-------------------------------------------
+let Number = 0 
+Number = Math.floor(Math.random() * 2) + 1;
+//------------------------------------------
+let Generator = function {
+switch (Number) 
+{
+    case 1:
+        if (pool_role.Sleeper < 3)
+        {
+            pool_role.Sleeper ++
             i++
-    }
+            break;
+        }
+        else{
+            break;
+        }
+        case 2: 
+        if (pool_role.Maf < 3)
+        {
+            pool_role.Maf++
+            i++
+            break;
+            
+        }
+        else {
+            break;
+        }
 }
-    while (i != 5);
-    //TODO доделай, а то прошлый ты заебался
+}
+console.log
+console.log ( Number + "real was")
+console.log ( pool_role.Maf)
+console.log ( pool_role.Sleeper)
+} while (i < 3 ); // here end
+
+// past you run out of time. do this next time 
