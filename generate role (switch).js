dff98ee78f3c632e1a1a2ec832cb6f25b45c28a3
Dumb1protogen <ghjcntvf@mail.ru>
2023-04-10 13:30:42 +0600

    its work, only all role left

--- generate role (switch).js
+++ generate role (switch).js
@@ -1,49 +1,40 @@
-let pool_role = {     
-    Sleeper: 0,  
-    Maf: 0        
-  };
+let pool ={
+    Peace: 0 ,
+    Maf: 0 ,
+    number: NaN, 
+};
+let counter = 0
 
+while (counter < 3) {
 
+pool.number = Math.floor(Math.random() * 3) + 1;
+console.log (pool.number + " number"); // debug
 
-  let i =  0
-do { // here start
+switch(pool.number) 
 
-
-
-//-------------------------------------------
-let Number = 0 
-Number = Math.floor(Math.random() * 2) + 1;
-//------------------------------------------
-let Generator = function {
-switch (Number) 
 {
-    case 1:
-        if (pool_role.Sleeper < 3)
+    case 1: 
+    if (pool.Peace < 3)
+    {
+        console.log ( counter + "you are Peace")
+        pool.Peace++
+        counter++
+    }
+    break; 
+    case 2:
+        if (pool.Maf < 3) 
         {
-            pool_role.Sleeper ++
-            i++
-            break;
+            console.log ( counter + "you are maf")
+            pool.Maf++
+            counter++
         }
-        else{
-            break;
-        }
-        case 2: 
-        if (pool_role.Maf < 3)
-        {
-            pool_role.Maf++
-            i++
-            break;
-            
-        }
-        else {
-            break;
-        }
+        break
 }
-}
-console.log
-console.log ( Number + "real was")
-console.log ( pool_role.Maf)
-console.log ( pool_role.Sleeper)
-} while (i < 3 ); // here end
 
-// past you run out of time. do this next time 
+}
+
+
+
+//console.log (pool.Peace + "peace") // debug
+//console.log (pool.Maf + "maf") // debug
+//console.log (counter + "counter") // debug
