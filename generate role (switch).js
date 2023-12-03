bb7c622c930b80ed759b0e8465b6e39605feaa77
Dumb1protogen <ghjcntvf@mail.ru>
2023-12-03 19:08:21 +0600

    added user shit

--- generate role (switch).js
+++ generate role (switch).js
@@ -1,7 +1,10 @@
+let debug = 0
+
 let Setting = {
     IsVillagerInGame:  false,
-    IsNitralsInGame: true,
-    IsMyOwnRoleeAdded: true,
+    IsNitralsInGame: false,
+    IsMyOwnRoleeAdded: false,
+    Players: 4,
 }
 let Kill = 0
 let pool ={
@@ -26,8 +29,115 @@ let pool ={
     Jani: 0,
 };
 let counter = 1
+//-----------------------SETINGS---------------
 
-while (counter < 10 + 1) {
+let SettingPlayersAdd = document.createElement('button');
+SettingPlayersAdd.textContent = '+1';
+SettingPlayersAdd.addEventListener('click', function() {
+  Setting.Players ++;
+  PlayersCount.textContent = 'Количесто игроков:' + " " + Setting.Players;
+});
+document.body.appendChild(SettingPlayersAdd);
+
+let SettingPlayersDeAdd = document.createElement('button');
+SettingPlayersDeAdd.textContent = '-1';
+SettingPlayersDeAdd.addEventListener('click', function() {
+  Setting.Players --    ;
+  PlayersCount.textContent = 'Количесто игроков:' + " " + Setting.Players;
+});
+document.body.appendChild(SettingPlayersDeAdd);
+
+let PlayersCount = document.createElement('button');
+PlayersCount.textContent = 'Количесто игроков:' + " " + Setting.Players;
+PlayersCount.addEventListener('click', function() {
+});
+document.body.appendChild(PlayersCount);
+
+
+let SettingOwn = document.createElement('button');
+SettingOwn.textContent = 'Будут ли в игре нейтральные: Нет';
+SettingOwn.addEventListener('click', function() {
+   if (Setting.IsMyOwnRoleeAdded == false) { 
+
+    SettingOwn.textContent = 'Будут ли в игре нейтральные: Да';
+    Setting.IsMyOwnRoleeAdded = true;
+
+   } 
+   else {
+    SettingOwn.textContent = 'Будут ли в игре нейтральеные: Нет';
+    Setting.IsMyOwnRoleeAdded = false;
+   }
+});
+document.body.appendChild(SettingOwn);
+
+
+let SettingNitrals = document.createElement('button');
+SettingNitrals.textContent = 'Будут ли в игре нейтральные: Нет';
+SettingNitrals.addEventListener('click', function() {
+   if (Setting.IsNitralsInGame == false) { 
+
+    SettingNitrals.textContent = 'Будут ли в игре нейтральные: Да';
+    Setting.IsNitralsInGame = true;
+
+   } 
+   else {
+    SettingNitrals.textContent = 'Будут ли в игре нейтральеные: Нет';
+    Setting.IsNitralsInGame = false;
+   }
+});
+document.body.appendChild(SettingNitrals);
+
+
+
+let SettingVillager = document.createElement('button');
+SettingVillager.textContent = 'Будут ли в игре мирные: Нет';
+SettingVillager.addEventListener('click', function() {
+   if (Setting.IsVillagerInGame == false) { 
+
+    SettingVillager.textContent = 'Будут ли в игре мирные: Да';
+    Setting.IsVillagerInGame = true;
+
+   } 
+   else {
+    SettingVillager.textContent = 'Будут ли в игре мирные: Нет';
+    Setting.IsVillagerInGame = false;
+   }
+});
+document.body.appendChild(SettingVillager);
+
+let a123 = document.createElement('button');
+a123.textContent = 'Click me';
+a123.addEventListener('click', function() {
+   if (i == 0) { 
+    a123.textContent = '0';
+    i = 1
+
+   } 
+   else {
+    a123.textContent = '1';
+    i = 0
+   }
+});
+
+//-----------------------SETINGS---------------
+
+
+let Start = document.createElement('button');
+Start.textContent = 'press here to start a game';
+Start.addEventListener('click', function() {
+
+   if (Setting.IsMyOwnRoleeAdded == false && Setting.IsNitralsInGame == false && Setting.IsVillagerInGame == false) {
+    alert("Притормози, ты собираешься выключить все. Я тебе установлю 4 игрока иначе ты будешь в бесконечной петле. Не забуть в следущий раз включить что-то")
+    Setting.Players = 4;
+   }
+
+
+
+
+
+//---------------------Loop start------------------
+
+while (counter < Setting.Players + 1) {
 
 pool.number = Math.floor(Math.random() * 15) + 1;
 console.log (pool.number + " number"); // debug
@@ -193,18 +303,34 @@ switch(pool.number)
             }
                 
                 break
+                default: 
+                    if (debug == 5) {
+                        counter = 9999;
+                        document.writeln ("Что-то пошло не так. Свяжитесь с создателем");
+                        alert ("Что то пошло не так");
+                    } 
+                    else {
+                        debug++
+                    }
 
 }
 
 
-}// loop end
+}
+// loop end
 
 
-if (pool.EvilSide == 0 )  
+
+
+
+
+
+
+/*if (pool.EvilSide == 0 )  
 {
     document.writeln ("НЕТ МАФИИ ДАВАЙ ПО НОВОЙ МИЩА");
     location.reload()
-}
+} */
 
 
 
@@ -214,3 +340,7 @@ document.writeln ("Всего мирных " + " " + pool.Vilager++ + "<br>");
 document.writeln ("Всего мафии" + " " + pool.EvilSide + "<br>");
 document.writeln ("Всего нейтралных ролей" + " " + pool.Neitrals + "<br>");
 document.writeln ("Всего роллей способных убивать ночью" + " " + Kill );
+
+});
+
+document.body.appendChild(Start);
